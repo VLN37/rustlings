cd5761f10242937fcbd7c619d588c027e81e69d7
VLN37 <jfpr1237@gmail.com>
2023-05-14 20:39:15 -0300

    rustlings 1-42

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -1,11 +1,35 @@
 // enums2.rs
 // Execute `rustlings hint enums2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+#[derive(Debug)]
+struct Point {
+    x: i32,
+    y: i32
+}
+
+struct PointTuple(u8, u8);
+
+impl Point {
+    fn build(x: i32, y: i32) -> Point {
+        Point {x, y}
+    }
+}
+
+struct Color {
+    red: i8,
+    green: i8,
+    blue: i8
+}
+
+struct ColorTuple(i32, i32, i32);
 
 #[derive(Debug)]
 enum Message {
     // TODO: define the different variants used below
+    Quit,
+    Echo(String),
+    Move{x: i32, y: i32},
+    ChangeColor(i32, i32, i32)
 }
 
 impl Message {
@@ -16,9 +40,9 @@ impl Message {
 
 fn main() {
     let messages = [
-        Message::Move { x: 10, y: 30 },
+        Message::Move {x: 10, y: 30},
         Message::Echo(String::from("hello world")),
-        Message::ChangeColor(200, 255, 255),
+        Message::ChangeColor(255, 255, 255),
         Message::Quit,
     ];
 
